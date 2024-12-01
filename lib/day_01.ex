b365ae996632c4f2f84fc75759ef984f28542c59
Emanuele DelBono <emanuele@codiceplastico.com>
2024-12-01 07:36:05 +0100

    day 1

--- lib/day_01.ex
+++ lib/day_01.ex
@@ -1,5 +1,27 @@
 defmodule Aoc.Day01 do
   def part_one(input) do
+    {l1, l2} = get_lists(input)
+
+    Enum.zip(Enum.sort(l1), Enum.sort(l2))
+    |> Enum.map(fn {e1, e2} -> abs(e1 - e2) end)
+    |> Enum.sum()
+  end
+
+  def part_two(input) do
+    {l1, l2} = get_lists(input)
+
+    freq = Enum.frequencies(l2)
+
+    Enum.map(l1, fn x -> x * Map.get(freq, x, 0) end)
+    |> Enum.sum()
+  end
+
+  defp get_lists(input) do
     input
+    |> InputFile.get_lines_string()
+    |> Enum.map(fn l -> String.split(l, "   ") end)
+    |> Enum.reduce({[], []}, fn [a, b], {l1, l2} ->
+      {[String.to_integer(a) | l1], [String.to_integer(b) | l2]}
+    end)
   end
 end
--- test/day_01_test.exs
+++ test/day_01_test.exs
@@ -4,8 +4,22 @@ defmodule Aoc.Day01Test do
   @real_input "./lib/inputs/day_01.txt"
 
   test "part one (test)" do
-    result = Aoc.Day01.part_one("ciao")
+    result = Aoc.Day01.part_one(@test_input)
+    assert result == 11
+  end
 
-    assert result == "ciao"
+  test "part one" do
+    result = Aoc.Day01.part_one(@real_input)
+    assert result == 1_197_984
+  end
+
+  test "part two (test)" do
+    result = Aoc.Day01.part_two(@test_input)
+    assert result == 31
+  end
+
+  test "part two" do
+    result = Aoc.Day01.part_two(@real_input)
+    assert result == 23_387_399
   end
 end

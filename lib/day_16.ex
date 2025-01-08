d811641d846a820c99c8bd20d95fdd9dd69a8a7c
Emanuele DelBono <emanuele@codiceplastico.com>
2025-01-08 16:54:09 +0100

    Day 16

--- lib/day_16.ex
+++ lib/day_16.ex
@@ -1,3 +1,7 @@
+defmodule Tree do
+  defstruct [:node, :score, :direction, :children]
+end
+
 defmodule Aoc.Day16 do
   def part_one(input) do
     map =
@@ -5,15 +9,83 @@ defmodule Aoc.Day16 do
       |> InputFile.get_file()
       |> build_map()
 
-    {start_pos, _} = Enum.find(map, fn {k, v} -> v == "S" end)
-    {end_pos, _} = Enum.find(map, fn {k, v} -> v == "E" end)
-    navigate(map, start_pos, "E", end_pos)
+    {start_pos, _} = Enum.find(map, fn {_k, v} -> v == "S" end)
+    {end_pos, _} = Enum.find(map, fn {_k, v} -> v == "E" end)
+    elem(navigate(map, start_pos, "E", end_pos), 0)
+  end
+
+  def part_two(input, score) do
+    map =
+      input
+      |> InputFile.get_file()
+      |> build_map()
+
+    {start_pos, _} = Enum.find(map, fn {_, v} -> v == "S" end)
+    {end_pos, _} = Enum.find(map, fn {_, v} -> v == "E" end)
+
+    build_tree(map, start_pos)
+    |> best_paths(score, end_pos)
+    |> List.flatten()
+    |> Enum.uniq()
+    |> Enum.count()
+  end
+
+  def best_paths(tree, score, dest) do
+    do_best_paths(tree, score, dest, [tree.node])
+  end
+
+  defp do_best_paths(%Tree{node: node, score: score, children: _}, target_score, dest, acc)
+       when node == dest and score == target_score do
+    [acc]
+  end
+
+  defp do_best_paths(%Tree{node: _, score: _, children: children}, target_score, dest, acc) do
+    Enum.flat_map(children, fn child ->
+      do_best_paths(child, target_score, dest, [child.node | acc])
+    end)
+  end
+
+  def build_tree(map, start) do
+    root = %Tree{node: start, score: 0, direction: "E", children: []}
+    visited = MapSet.new([start])
+    do_build_tree(map, root, visited)
+  end
+
+  defp do_build_tree(map, node, visited) do
+    children =
+      get_valid_moves(map, node.node, node.direction)
+      |> Enum.reject(fn {pos, _d, _s} -> MapSet.member?(visited, pos) end)
+      |> Enum.map(fn {pos, direction, move_score} ->
+        child = %Tree{
+          node: pos,
+          score: node.score + move_score,
+          direction: direction,
+          children: []
+        }
+
+        new_visited = MapSet.put(visited, pos)
+        do_build_tree(map, child, new_visited)
+      end)
+
+    %{node | children: children}
+  end
+
+  def get_valid_moves(map, {x, y}, direction) do
+    Enum.reduce([{-1, 0}, {1, 0}, {0, -1}, {0, 1}], [], fn {dx, dy}, acc ->
+      next_pos = {x + dx, y + dy}
+
+      case map[next_pos] do
+        "." -> [{next_pos, dir({dx, dy}), score({dx, dy}, direction)} | acc]
+        "E" -> [{next_pos, dir({dx, dy}), score({dx, dy}, direction)} | acc]
+        _ -> acc
+      end
+    end)
   end
 
   defp navigate(map, pos, dir, end_p) do
     visited = MapSet.new()
-    {score, _, _} = do_find(map, [{0, pos, dir}], visited, end_p)
-    score
+    {score, paths, v} = do_find(map, [{0, pos, dir}], visited, end_p)
+    {score, paths, v}
   end
 
   defp do_find(map, queue, visited, end_p) do
@@ -24,7 +96,7 @@ defmodule Aoc.Day16 do
     queue = Enum.reject(queue, fn xx -> xx == {w, {x, y}, d} end)
 
     if {x, y} == end_p do
-      {w, {x, y}, d}
+      {w, {x, y}, visited}
     else
       visited = MapSet.put(visited, {x, y})
 
@@ -40,10 +112,9 @@ defmodule Aoc.Day16 do
               acc
 
             true ->
-              new_weight = w + score({dx, dy}, d)
-              new_dir = new_dir({dx, dy})
+              new_score = w + score({dx, dy}, d)
 
-              [{new_weight, next_pos, new_dir} | acc]
+              [{new_score, next_pos, dir({dx, dy})} | acc]
           end
         end)
 
@@ -53,26 +124,15 @@ defmodule Aoc.Day16 do
     end
   end
 
-  defp new_dir({1, 0}), do: "E"
-  defp new_dir({-1, 0}), do: "W"
-  defp new_dir({0, 1}), do: "S"
-  defp new_dir({0, -1}), do: "N"
+  defp dir({1, 0}), do: "E"
+  defp dir({-1, 0}), do: "W"
+  defp dir({0, 1}), do: "S"
+  defp dir({0, -1}), do: "N"
 
-  defp score({_, 0}, dir) when dir in ["E", "W"] do
-    1
-  end
-
-  defp score({_, 0}, dir) when dir in ["N", "S"] do
-    1001
-  end
-
-  defp score({0, _}, dir) when dir in ["N", "S"] do
-    1
-  end
-
-  defp score({0, _}, dir) when dir in ["E", "W"] do
-    1001
-  end
+  defp score({_, 0}, dir) when dir in ["E", "W"], do: 1
+  defp score({_, 0}, dir) when dir in ["N", "S"], do: 1001
+  defp score({0, _}, dir) when dir in ["N", "S"], do: 1
+  defp score({0, _}, dir) when dir in ["E", "W"], do: 1001
 
   defp build_map(input) do
     input
@@ -93,16 +153,5 @@ defmodule Aoc.Day16 do
         acc
       end
     end)
-  end
-
-  def part_two(input) do
-    map =
-      input
-      |> InputFile.get_file()
-      |> build_map()
-
-    {start_pos, _} = Enum.find(map, fn {k, v} -> v == "S" end)
-    {end_pos, _} = Enum.find(map, fn {k, v} -> v == "E" end)
-    navigate(map, start_pos, "E", end_pos)
   end
 end
--- test/day_16_test.exs
+++ test/day_16_test.exs
@@ -3,6 +3,7 @@ defmodule Aoc.Day16Test do
   @test_input "./test/inputs/day_16.txt"
   @real_input "./lib/inputs/day_16.txt"
 
+  # @tag timeout: :infinity
   test "part one (test)" do
     input = Path.absname(@test_input)
     result = Aoc.Day16.part_one(input)
@@ -10,23 +11,25 @@ defmodule Aoc.Day16Test do
     assert result == 7036
   end
 
+  @tag timeout: :infinity
   test "part one" do
     input = Path.absname(@real_input)
     result = Aoc.Day16.part_one(input)
     assert result == 91464
   end
 
+  @tag timeout: :infinity
   test "part two (test)" do
     input = Path.absname(@test_input)
-    result = Aoc.Day16.part_two(input)
+    result = Aoc.Day16.part_two(input, 7036)
 
     assert result == 45
   end
 
-  @tag :skip
+  @tag timeout: :infinity
   test "part two" do
     input = Path.absname(@real_input)
-    result = Aoc.Day16.part_two(input)
-    assert result == input
+    result = Aoc.Day16.part_two(input, 91464)
+    assert result == 494
   end
 end
